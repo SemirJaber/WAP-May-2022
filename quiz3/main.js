d15b9a49643575df8e12346f8d6febbc6aa9d0c3
SemirJaber <jabersemir@gmail.com>
2022-05-10 16:44:35 -0500

    animation

---
+++ quiz3/main.js
@@ -0,0 +1,13 @@
+
+    
+    function kitty(){
+        document.getElementById('myImage').src="kitty.jpg";
+        document.getElementById('message').innerHTML="Furry Friend of the Day";
+    }
+      
+    function puppy(){
+        document.getElementById('myImage')
+        .src="puppy.jpg";
+        document.getElementById('message')
+        .innerHTML="Furry Friend of the Day";
+    }
